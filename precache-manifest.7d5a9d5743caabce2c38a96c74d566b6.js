a5af4868ead27effcefb185a3d74ef64971362aa
jessicaBuenfil <jessibuenfilsolis@gmail.com>
2020-05-10 16:49:36 -0500

    Updates

---
+++ precache-manifest.7d5a9d5743caabce2c38a96c74d566b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42310e94c3f8ee1a96a9f9c040fcd255",
+    "url": "/CounterApp/index.html"
+  },
+  {
+    "revision": "51aa18eaac4cb2685bfa",
+    "url": "/CounterApp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a932412d6e0126c43294",
+    "url": "/CounterApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51aa18eaac4cb2685bfa",
+    "url": "/CounterApp/static/js/2.ebaf53e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterApp/static/js/2.ebaf53e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a932412d6e0126c43294",
+    "url": "/CounterApp/static/js/main.cd336e93.chunk.js"
+  },
+  {
+    "revision": "3a6ec22bc134b7cab911",
+    "url": "/CounterApp/static/js/runtime-main.10483ded.js"
+  }
+]);
